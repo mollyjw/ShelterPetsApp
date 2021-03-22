bdcb24fe4eb26c1a6917ad50f5e8d16225c5e29a
Molly J. Whitener <mollyjwhitener@WWPROGRAMMING.COM>
2021-03-22 10:24:42 -0500

    first REAL attempt at front end

--- ShelterPets/Startup.cs
+++ ShelterPets/Startup.cs
@@ -1,17 +1,19 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using ShelterPets.Data;
 using ShelterPets.Models;
 
@@ -30,15 +32,34 @@ namespace ShelterPets
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ShelterPetsContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ShelterPetsContext")));
+            // services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
+            services.AddSpaStaticFiles(configuration: options => { options.RootPath = "wwwroot"; });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            // services.AddMvc(option => option.EnableEndpointRouting = false);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ShelterPets", Version = "v1" });
             });
-
-            services.AddDbContext<ShelterPetsContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ShelterPetsContext")));
+            services.AddCors(options =>
+            {
+                options.AddPolicy("VueCorsPolicy", builder =>
+                {
+                    builder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                    .WithOrigins("https://localhost:5001");
+                });
+            });
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+                {
+                    options.Authority = Configuration["Okta:Authority"];
+                    options.Audience = "api://default";
+                });
+            }      
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
